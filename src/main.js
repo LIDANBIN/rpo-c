a6fc337780021c007d0d00e47b6446e87a4d0b35
LIDANBIN <straberry2@outlook.com>
2018-05-10 14:40:42 +0800

    --

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './common/style/index.styl'
-import './common/scripts/flexible'
+import '&/style/index.styl'
+import '&/scripts/flexible'
 Vue.config.productionTip = false
 
 new Vue({
