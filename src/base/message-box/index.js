932471f8c7d9d2db9dff4743875385aa283a3758
LIDANBIN <straberry2@outlook.com>
2018-06-08 17:58:49 +0800

    --

--- src/base/message-box/index.js
+++ src/base/message-box/index.js
@@ -1,3 +1,26 @@
 import Vue from 'vue'
 import MessageBox from './message-box'
+import tool from "&/scripts/tools";
 
+let MessageBoxConstructor = Vue.extend(MessageBox)
+
+let createMessageBox = () => {
+	return new MessageBoxConstructor({
+		el: document.createElement('div')
+	})
+}
+
+let getInstanceMessageBox = tool.getInstance(createMessageBox)
+
+let removeDom = (event) => {
+	if (event.target.parentNode) {
+		event.target.parentNode.removeChild(event.target)
+	}
+}
+
+ToastConstructor.prototype.close = function () {
+	this.visible = false;
+	this.$el.addEventListener('transitionend', removeDom);
+	this.closed = true;
+	return this;
+}
